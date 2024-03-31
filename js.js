d1ae874b85bc8c8e3490cc81ff82723dfe9db4bc
phanuel tsopze <146930921+alibia-phanuel@users.noreply.github.com>
2024-03-31 01:48:18 +0100

    portfolio UX
         Il étais question pour moi de reproduire le portfolio d`un UX
                      Designer et développeur avec des effets d`animation et
                      apparition au scool grâce au JavaScript

---
+++ js.js
@@ -0,0 +1,6 @@
+var menu = document.querySelector(".bars");
+var addClass = document.querySelector(".menu");
+
+menu.addEventListener("click", function () {
+  addClass.classList.toggle("show");
+});
